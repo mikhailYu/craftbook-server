51e9d3c49685d48bf1433eac7367bcb2479f0d0c
Mikhail <mikhailyu@hotmail.co.nz>
2023-06-21 17:13:07 +1200

    cookie fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
-var session = require("express-session");
+var session = require("cookie-session");
 var multer = require("multer");
 
 var router = express.Router();
