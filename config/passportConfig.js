794ba89b2a8e2e8d8637d3c7e2ad6235b1fae23f
Mikhail <mikhailyu@hotmail.co.nz>
2023-06-21 17:44:27 +1200

    done

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -5,15 +5,15 @@ require("dotenv").config();
 const User = require("../models/userModel");
 
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  return done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
-    done(null, user);
+    return done(null, user);
   } catch (err) {
-    done(err);
+    return done(err);
   }
 });
 
