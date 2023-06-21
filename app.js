7ecbb6f7607fc7f9a88e14e1cde421dbac1af12b
Mikhail <mikhailyu@hotmail.co.nz>
2023-06-21 17:56:52 +1200

    post

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(
     secret: "secret",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 86400000, secure: true, sameSite: "none" },
+    cookie: { maxAge: 86400000, secure: true },
   })
 );
 app.use(passport.initialize());
