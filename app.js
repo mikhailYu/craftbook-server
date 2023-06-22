34fd685086d3733dc27ce86bca2dac6c2988999b
Mikhail <mikhailyu@hotmail.co.nz>
2023-06-22 21:19:20 +1200

    x

--- app.js
+++ app.js
@@ -101,7 +101,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // might have to delete this, passport related
 app.use((request, response, next) => {
-  response.setHeader("Access-Control-Allow-Origin", "*");
   response.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
