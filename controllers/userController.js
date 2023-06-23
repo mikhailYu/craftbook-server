77ef236df41af6702c94bfbcc5af16f5fea87b50
Mikhail <mikhailyu@hotmail.co.nz>
2023-06-23 13:35:28 +1200

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const uniqid = require("uniqid");
 
 exports.authUser = passport.authenticate("google", {
-  scope: ["email profile"],
+  scope: ["openid", "email", "profile"],
 });
 
 exports.authLogout = async (req, res, next) => {
