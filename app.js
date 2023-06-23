4b0a7bd3ed6316be9cb78e39915071d545240c00
Mikhail <mikhailyu@hotmail.co.nz>
2023-06-23 15:07:58 +1200

    fff

--- app.js
+++ app.js
@@ -68,12 +68,12 @@ app.use(
   session({
     secret: "secret",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
-      maxAge: 86400000,
+      maxAge: 8640000,
       SameSite: "None",
       secure: false,
-      httpOnly: true,
+      httpOnly: false,
     },
   })
 );
